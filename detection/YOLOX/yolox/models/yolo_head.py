f3b92aefa510c5e970efd81bc92449f2937ab33e
KKKSQJ <739677819@qq.com>
2021-11-10 13:45:06 +0800

    add focal loss

--- detection/YOLOX/yolox/models/yolo_head.py
+++ detection/YOLOX/yolox/models/yolo_head.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from yolox.utils import bboxes_iou
 
-from .losses import IOUloss
+from .losses import IOUloss, FocalLoss
 from .network_blocks import BaseConv, DWConv
 
 
@@ -124,7 +124,8 @@ class YOLOXHead(nn.Module):
 
         self.use_l1 = False
         self.l1_loss = nn.L1Loss(reduction="none")
-        self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
+        # self.bcewithlog_loss = nn.BCEWithLogitsLoss(reduction="none")
+        self.bcewithlog_loss = FocalLoss(gamma=2.0, alpha=0.25, reduction='none')
         self.iou_loss = IOUloss(reduction="none")
         self.strides = strides
         self.grids = [torch.zeros(1)] * len(in_channels)
