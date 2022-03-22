0f0db3811bbf7707b8e2195508d4e3c628f8f1c7
KKKSQJ <739677819@qq.com>
2022-03-22 21:01:43 +0800

    Modified   Image_segmentation/FCN/predict.py

--- Image_segmentation/FCN/predict.py
+++ Image_segmentation/FCN/predict.py
@@ -1,13 +1,19 @@
+import argparse
+import glob
 import os
 import time
 import json
 
+import matplotlib.pyplot as plt
 import torch
 from torchvision import transforms
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 
 from models import fcn_resnet50
+
+IMG_FORMATS = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp', 'mpo']
 
 
 def time_synchronized():
@@ -15,30 +21,36 @@ def time_synchronized():
     return time.time()
 
 
-def main():
-    aux = False  # inference time not need aux_classifier
-    classes = 20
-    weights_path = "./save_weights/model_0.pth"
-    img_path = "./data/test1.jpg"
-    palette_path = "./utils/palette.json"
-    assert os.path.exists(weights_path), f"weights {weights_path} not found."
-    assert os.path.exists(img_path), f"image {img_path} not found."
+@torch.no_grad()
+def run(
+        classes=1,
+        weights='best_model.pth',
+        source='./data/test',
+        use_cuda=True,
+        view_img=False,
+        save=True,
+        palette_path="./utils/palette.json",
+        out_path='./mask'
+):
+    assert os.path.exists(weights), f"weights {weights} not found."
+    assert os.path.exists(source), f"image {source} not found."
     assert os.path.exists(palette_path), f"palette {palette_path} not found."
+    os.makedirs(out_path, exist_ok=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+    print("using {} device.".format(device))
+
     with open(palette_path, "rb") as f:
         pallette_dict = json.load(f)
         pallette = []
         for v in pallette_dict.values():
             pallette += v
 
-    # get devices
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device.".format(device))
-
     # create model
-    model = fcn_resnet50(aux=aux, num_classes=classes+1)
-
+    aux = False  # inference time not need aux_classifier
+    model = fcn_resnet50(aux=aux, num_classes=classes + 1)
     # delete weights about aux_classifier
-    weights_dict = torch.load(weights_path, map_location='cpu')['model']
+    weights_dict = torch.load(weights, map_location='cpu')['model']
     for k in list(weights_dict.keys()):
         if "aux" in k:
             del weights_dict[k]
@@ -46,25 +58,35 @@ def main():
     # load weights
     model.load_state_dict(weights_dict)
     model.to(device)
+    model.eval()
 
-    # load image
-    original_img = Image.open(img_path)
+    # run once
+    y = model(torch.rand(1, 3, 256, 256).to(device))
 
     # from pil image to tensor and normalize
-    data_transform = transforms.Compose([transforms.Resize(520),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                                                              std=(0.229, 0.224, 0.225))])
-    img = data_transform(original_img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
+    data_transform = transforms.Compose([  # transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                             std=(0.229, 0.224, 0.225))])
 
-    model.eval()  # 进入验证模式
-    with torch.no_grad():
-        # init model
-        img_height, img_width = img.shape[-2:]
-        init_img = torch.zeros((1, 3, img_height, img_width), device=device)
-        model(init_img)
+    # load img
+    assert os.path.exists(source), "data source: {} does not exists".format(source)
+    if os.path.isdir(source):
+        files = sorted(glob.glob(os.path.join(source, '*.*')))
+    elif os.path.isfile(source):
+        files = [source]
+    else:
+        raise Exception(f'ERROR: {source} does not exist')
+
+    images = [x for x in files if x.split('.')[-1].lower() in IMG_FORMATS]
+    images = tqdm(images)
+    image_list = []
+    for img_path in images:
+        img_name = img_path.split(os.sep)[-1][:-4]
+        original_img = Image.open(img_path)
+        img = data_transform(original_img)
+        # expand batch dimension
+        img = torch.unsqueeze(img, dim=0)
 
         t_start = time_synchronized()
         output = model(img.to(device))
@@ -75,9 +97,23 @@ def main():
         prediction = prediction.to("cpu").numpy().astype(np.uint8)
         mask = Image.fromarray(prediction)
         mask.putpalette(pallette)
-        mask.save("test_result.png")
+        if view_img:
+            # mask.show()
+            plt.imshow(mask)
+            plt.show()
+        if save:
+            mask.save(os.path.join(out_path, "{}.png".format(img_name)))
 
 
 if __name__ == '__main__':
-    main()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='best_model.pth', help='the model path')
+    parser.add_argument('--source', type=str, default='/data/test', help='test data path')
+    parser.add_argument('--classes', type=int, default=1, help='num of classes')
+    parser.add_argument('--use-cuda', type=bool, default=True)
+    parser.add_argument('--view-img', type=bool, default=False)
+    parser.add_argument('-s', '--save', type=bool, default=True)
+    parser.add_argument('--out_path', type=str, default='runs/result', help='output path')
+    parser.add_argument('--palette_path', type=str, default='palette.json')
+    opt = parser.parse_args()
+    run(**vars(opt))
